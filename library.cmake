ac2b562c22b158981928afa71cc56d05f9c7eaf8
Pranav Suri <pranavsuri@icloud.com>
2026-03-13 21:03:53 +0100

    delete main.py and update library.cmake

---
+++ library.cmake
@@ -0,0 +1,7 @@
+# library.cmake
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Sx1280Radio/ThirdParty/sx128x")
+# add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Sx1280Radio/Link")
+# add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Sx1280Radio/Hal")
+# add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Sx1280Radio/Device")
+# add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Sx1280Radio/Components/LoraComAdapter")
+# add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Sx1280Radio/test/unit")
